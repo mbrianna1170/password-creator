7a7621520ad4402bac6531fef6a2c51d76d6ad78
mbrianna1170 <brianna.miller09@yahoo.com>
2021-05-30 22:39:03 -0500

    created length criteria for password

--- Develop/script.js
+++ Develop/script.js
@@ -1,65 +1,26 @@
-// create new variables for password
-var upperCase = [ 
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z',
-];
+var upperCaseCharacters = "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z";
+// console.log(upperCaseCharacters);
+var lowerCaseCharacters = "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z";
+// console.log(lowerCaseCharacters);
+var numberCharacters = "1, 2, 3, 4, 5, 6, 7, 8, 9";
+// console.log(numberCharacters);
+var specialCharacters = "!, @, #, $, %, &, *, ?";
+// console.log(specialCharacters);
 
-var lowerCase = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z',
-];
- 
-var numberCharacters = ['0','1','2','3','4','5','6','7','8','9'];
+function generatePassword() {
+  passwordLength = prompt('Please enter the amount of characters you would like in your password. Choose between 8-128 characters.');
+  if (passwordLength < 8 || passwordLength > 128) {
+    return alert("Please choose a number between 8-128.")
+  } else if (isNaN(passwordLength)) {
+    passwordLength = alert('Please choose a number between 8-128.');
+  }  
+  else {
+    confirm('Your password will be' + ' ' + passwordLength + ' ' + 'characters long.');
+  }
+};
 
-var specialCharacters = ['!','#','$','%','&','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[',']','^','_','`','{','|','}','~'];
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
